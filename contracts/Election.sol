2b4bdcb199c879b1df595b0727a4b27b975e45f1
vibbsdod <vibbs.dod@gmail.com>
2019-01-20 22:23:20 +0530

    added code and test cases from the tutorial, need to add the special identification ID of the each account. Like aadhar or somethign for India

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -32,4 +32,31 @@ contract Election {
         candidates[candidatesCount] = Candidate (candidatesCount, _name, 0);
     }
 
+
+    //Voting part of the code:
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+
 }
--- src/js/app.js
+++ src/js/app.js
@@ -81,16 +81,17 @@ App = {
 
       for (var i = 1; i <= candidatesCount; i++) {
         electionInstance.candidates(i).then(function(candidate) {
+          // console.log(candidate);
           var id = candidate[0];
           var name = candidate[1];
           var voteCount = candidate[2];
 
           // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
           candidatesResults.append(candidateTemplate);
 
           // Render candidate ballot option
-          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>";
           candidatesSelect.append(candidateOption);
         });
       }
