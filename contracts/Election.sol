21a3389382e270aa67d4f17297d1b08ef9326971
vibbsdod <vibbs.dod@gmail.com>
2019-01-20 21:38:40 +0530

    added some mroe code and wrote some test cases along with some html file from the tutorial

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,35 @@
-pragma solidity ^0.5.0;
+pragma solidity >0.4.2 <0.6.0;
 
 contract Election {
     // Constructor, with store and read candidate
     string public candidate;
 
+    //Basic Candidate structure
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+        // string partyName;
+        // string adharNumber; 
+        // can be a generic UUID which could corresponds to PAN, etc
+    }
+
+    //Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+
+    // total number of Candidates
+    uint public candidatesCount;
+
     constructor() public{
-        candidate = "Candidate - 1";
+        addCondidate("BJP");
+        addCondidate("Congress");
+    }
+
+    // add Condidate
+    function addCondidate(string memory _name) private {
+        candidatesCount ++;
+        
+        candidates[candidatesCount] = Candidate (candidatesCount, _name, 0);
     }
 
 }
